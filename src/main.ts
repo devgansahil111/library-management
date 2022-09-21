a799277756c3e08348925f356254f5595bbf7952
mayank <mayankgoyal115@gmail.com>
2022-09-21 16:17:05 +0530

    done

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,12 @@
-import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import * as cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const config = new DocumentBuilder()
-    .setTitle('Library Management System')
-    .setDescription('Library Management APIs')
-    .setVersion('1.6.0')
-    // .addBearerAuth()
-    .addTag('LMS')
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
-  app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000, () => {
-    console.log(`Server running on port : 3000 `);
-  });
+  
+  app.use(cookieParser());
+  await app.listen(3000);
 }
 bootstrap();
+
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,85 +1,67 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Param,
-  Delete,
-  Res,
-  UseGuards,
-  Put,
-} from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Res, UseGuards, Put, NotFoundException } from '@nestjs/common';
 import { UserService } from './user.service';
-import { catchError, map, Observable, of } from 'rxjs';
+import { catchError, from, map, Observable, of } from 'rxjs';
 import { Role, User } from './model/user.interface';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-guard';
 import { RolesGuard } from 'src/auth/guards/roles.guard';
 import { hasRoles } from 'src/auth/decorators/roles.decorator';
 import { UserIsUserGuard } from 'src/auth/guards/UserIsUser.guard';
+import { Book } from 'src/book/entities/book.interface';
 
 @Controller('users')
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+    constructor(private readonly userService: UserService) { }
 
-  @Post('signup')
-  create(@Body() user: User): Observable<User | Object> {
-    return this.userService.create(user).pipe(
-      map((user: User) => user),
-      catchError((err) => of({ error: err.message })),
-    );
-  }
- // No autherization required here
-  @Post('login')
-  login(@Body() user: User): Observable<Object> {
-    return this.userService.login(user).pipe(
-      map((jwt: string) => {
-        return { access_token: jwt };
-      }),
-    );
-  }
+    @Post('signup')
+    create(@Body() user: User): Observable<User | Object> {
+        return this.userService.create(user).pipe(
+            map((user: User) => user),
+            catchError(err => of({ error: err.message }))
+        );
+    }
 
-  @Get(':id')
-  findOne(@Param() params): Observable<User> {
-    return this.userService.findOne(params.id);
-  }
+    @Post('login')
+    login(@Body() user: User): Observable<Object> {
+        return this.userService.login(user).pipe(
+            map((jwt: string) => {
+                return { access_token: jwt };
+            })
+        )
+    }
 
-  @hasRoles(Role.ADMIN)
-  @UseGuards(JwtAuthGuard, RolesGuard)
-  @Get()
-  findAll(): Observable<User[]> {
-    return this.userService.findAll();
-  }
+    @Get(':id')
+    findOne(@Param() params): Observable<User> {
+        return this.userService.findOne(params.id);
+    }
 
-  @hasRoles(Role.ADMIN)
-  @UseGuards(JwtAuthGuard, RolesGuard)
-  @Delete(':id')
-  deleteOne(@Param('id') id: string): Observable<User> {
-    return this.userService.deleteOne(id);
-  }
 
-  //   @UseGuards(JwtAuthGuard, UserIsUserGuard)
-  //   @Put(':id')
-  //   updateOne(@Param('id') id: string, @Body() user: User): Observable<any> {
-  //     return this.userService.updateOne(id, user);
-  //   }
+    @hasRoles(Role.ADMIN)
+    @UseGuards(JwtAuthGuard, RolesGuard)
+    @Get()
+    findAll(): Observable<User[]> {
+        return this.userService.findAll();
+    }
 
-  @hasRoles(Role.ADMIN)
-  @UseGuards(JwtAuthGuard, RolesGuard)
-  @Put(':id/role')
-  updateRoleOfUser(
-    @Param('id') id: string,
-    @Body() user: User,
-  ): Observable<User> {
-    return this.userService.updateRoleOfUser(id, user);
-  }
 
-  // @hasRoles(Role.ADMIN)
-  // @UseGuards(JwtAuthGuard, RolesGuard)
-  // @Post('/borrow')
-  // borrowBook(@Body() user: User) {
-  //   return this.userService.borrowBook(user).pipe(
-  //     map((user: User) => user),
-  //     catchError((err) => of({ error: err.message })),
-  //   );
-  // }
+    @hasRoles(Role.ADMIN)
+    @UseGuards(JwtAuthGuard, RolesGuard)
+    @Delete(':id')
+    deleteOne(@Param('id') id: string): Observable<User> {
+        return this.userService.deleteOne(id)
+    }
+
+    @UseGuards(JwtAuthGuard, UserIsUserGuard)
+    @Put(':id')
+    updateOne(@Param('id') id: string, @Body() user: User): Observable<any> {
+        return this.userService.updateOne(id, user)
+    }
+
+    @hasRoles(Role.ADMIN)
+    @UseGuards(JwtAuthGuard, RolesGuard)
+    @Put(':id/role')
+    updateRoleOfUser(@Param('id') id: string, @Body() user: User): Observable<User> {
+        return this.userService.updateRoleOfUser(id, user)
+    }
+    
+  
 }
